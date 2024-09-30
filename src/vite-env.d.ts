cff343ef92a43177c30fd87b931391081deb27cd
kanghanstar <kanghanstar@gmail.com>
2024-09-30 17:17:03 +0900

    Default setting

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_NODE_ENV: string;
+    // 다른 환경 변수들에 대한 타입 정의
+    readonly VITE_API_URL: string;
+    readonly VITE_STORE_ID: string;
+    readonly VITE_CHANNEL_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
